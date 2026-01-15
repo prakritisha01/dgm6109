421ccb172e4f17b5519b788ef446c4fe878c00fa
prakritisha01 <prakritisha01@gmail.com>
2026-01-15 02:43:19 -0800

    Uploaded index file
    This is lab1 homework

---
+++ lab1/temp-converter.js
@@ -0,0 +1,12 @@
+let fahrenheit = 74
+// Average temperature in Delhi in December 2025
+
+let celsius = (fahrenheit - 32) * 5/9
+
+let kelvin = (fahrenheit + 459.67) * 5/9
+
+console.log("Temperature (fahrenheit):" + fahrenheit)
+
+console.log("Temperature (celsius): " + celsius)
+
+console.log("Temperature (kelvin): " + kelvin)
