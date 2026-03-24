b1211860766e43d68d86052999a767830a98955d
prakritisha01 <prakritisha01@gmail.com>
2026-03-24 15:56:05 -0700

    update

--- term/draft2/main.js
+++ term/draft2/main.js
@@ -4,13 +4,14 @@ let svgWidth= 600
 let svgHeight = 400
 let margin = 40
 
-let maxScreenTime = 155
+/* maximum values used in the scale domains*/
+let maxScreenTime = 160
 let maxSleepQuality = 5
 
 d3.select("#container")
 .style("width", svgWidth + "px")
 
-/*SVG canvas */
+/*SVG canvas - main drawing area for the visualisation*/
 let svg= d3.select("#canvas")
     .append("svg")
     .attr("width", svgWidth)
@@ -21,9 +22,9 @@ svg.append("rect")
    .attr("fill", "none")
    .attr("stroke","black")
    .attr("width", svgWidth)
-   .attr("height", svgHeight)
+   .attr("height", svgHeight) 
 
-/*margin border*/
+/*margin border - inner plotting area */
 svg.append("rect")
    .attr("fill", "none")
    .attr("x", margin)
@@ -31,88 +32,109 @@ svg.append("rect")
    .attr("width", svgWidth - margin*2)
    .attr("height", svgHeight - margin*2)
 
-/*Dataset*/
-let dataset = [
-    {screenTime: 42, sleepQuality: 3, totalSleep: 7}, 
-    {screenTime: 15, sleepQuality: 4, totalSleep: 7.5},
-    {screenTime: 57, sleepQuality: 2, totalSleep: 6},
-    {screenTime: 35, sleepQuality: 3, totalSleep: 6},
-    {screenTime: 40, sleepQuality: 3, totalSleep: 3},
-    {screenTime: 155, sleepQuality: 1, totalSleep: 4},
-    {screenTime: 48, sleepQuality: 3, totalSleep: 6.5},
-    {screenTime: 62, sleepQuality: 2, totalSleep: 6},
-    {screenTime: 28, sleepQuality: 4, totalSleep: 7},
-    {screenTime: 33, sleepQuality: 4, totalSleep: 7},
-    {screenTime: 90, sleepQuality: 5, totalSleep: 9},
-    {screenTime: 120, sleepQuality: 2, totalSleep: 5},
-    {screenTime: 20, sleepQuality: 5, totalSleep: 8},
-    {screenTime: 75, sleepQuality: 3, totalSleep: 6},
-    {screenTime: 140, sleepQuality: 1, totalSleep: 4},
-    {screenTime: 30, sleepQuality: 4, totalSleep: 7},
-    {screenTime: 25, sleepQuality: 4, totalSleep: 7.5},
-    {screenTime: 85, sleepQuality: 2, totalSleep: 5.5},
-    {screenTime: 38, sleepQuality: 4, totalSleep: 7},
-    {screenTime: 110, sleepQuality: 2, totalSleep: 4.5},
-    {screenTime: 32, sleepQuality: 4, totalSleep: 7},
+/* dataset */
+
+// (async function () {
+//    data= await d3.json("dataset.json").then(buildVisualization)
+//    console.log("here")
+// })(); 
+
+let dataset = [ {screenTime: 42, sleepQuality: 3, totalSleep: 7}, 
+   {screenTime: 15, sleepQuality: 4, totalSleep: 7.5}, 
+   {screenTime: 57, sleepQuality: 2, totalSleep: 6}, 
+   {screenTime: 35, sleepQuality: 3, totalSleep: 6}, 
+   {screenTime: 40, sleepQuality: 3, totalSleep: 3}, 
+   {screenTime: 155, sleepQuality: 1, totalSleep: 4}, 
+   {screenTime: 48, sleepQuality: 3, totalSleep: 6.5}, 
+   {screenTime: 62, sleepQuality: 2, totalSleep: 6}, 
+   {screenTime: 28, sleepQuality: 4, totalSleep: 7}, 
+   {screenTime: 33, sleepQuality: 4, totalSleep: 7}, 
+   {screenTime: 90, sleepQuality: 5, totalSleep: 9}, 
+   {screenTime: 120, sleepQuality: 2, totalSleep: 5}, 
+   {screenTime: 20, sleepQuality: 5, totalSleep: 8}, 
+   {screenTime: 75, sleepQuality: 3, totalSleep: 6}, 
+   {screenTime: 140, sleepQuality: 1, totalSleep: 4}, 
+   {screenTime: 30, sleepQuality: 4, totalSleep: 7}, 
+   {screenTime: 25, sleepQuality: 4, totalSleep: 7.5}, 
+   {screenTime: 85, sleepQuality: 2, totalSleep: 5.5}, 
+   {screenTime: 38, sleepQuality: 4, totalSleep: 7}, 
+   {screenTime: 110, sleepQuality: 2, totalSleep: 4.5}, 
+   {screenTime: 32, sleepQuality: 4, totalSleep: 7}, 
 ]
 
 
 /*Scales*/
-
-let xScale = d3.scaleLinear()
+let xScale = d3.scaleLinear() // chose linear scales because both variables are numeric values
 .domain([0,maxScreenTime])
 .range([margin, svgWidth - margin])
 
 let yScale = d3.scaleLinear()
 .domain([0,maxSleepQuality])
-.range([svgHeight - margin, margin])
+.range([svgHeight - margin, margin]) // reversed because higher values should appear higher
+
 
 /* axis lines */
-svg.append("line")
-   .attr("x1", margin)
-   .attr("x2", svgWidth - margin)
-   .attr("y1", svgHeight - margin)
-   .attr("y2", svgHeight - margin)
-   .attr("stroke", "black")
+let xAxis = d3.axisBottom(xScale)
 
-svg.append("line")
-   .attr("x1", margin)
-   .attr("x2", margin)
-   .attr("y1", margin)
-   .attr("y2", svgHeight - margin)
-   .attr("stroke", "black")
+let yAxis = d3.axisLeft(yScale)
+   .tickValues([1,2,3,4,5]) // it was shown as decimals 1.0, 2.0, 3.0.. 
+   .tickFormat(d3.format("d")) /* so I took help from geeksforgeeks.org and found this function where 
+                                 d3.format() formats the number and "d" displays the number as whole number*/
 
-/* axis numbers */
+svg.append("g")
+   .attr("transform", "translate(0," + (svgHeight - margin) + ")")
+   .call(xAxis)
 
-for (let i = 1; i <= 5; i++) {
-svg.append("text")
-   .attr("x", margin - 10)
-   .attr("y", yScale(i))
-   .attr("text-anchor", "end")
-   .attr("alignment-baseline", "middle")
-   .text(i)
+svg.append("g")
+   .attr("transform", "translate(" + margin + ", 0)")
+   .call(yAxis)
 
-} 
 
-for (let i = 0; i <= 160; i += 20) {
+// svg.append("line")
+//    .attr("x1", margin)
+//    .attr("x2", svgWidth - margin)
+//    .attr("y1", svgHeight - margin)
+//    .attr("y2", svgHeight - margin)
+//    .attr("stroke", "black")
 
-svg.append("text")
-   .attr("x", xScale(i))
-   .attr("y", svgHeight - margin + 15)
-   .attr("text-anchor", "middle")
-   .text(i)
+// svg.append("line")
+//    .attr("x1", margin) // x values stays
+//    .attr("x2", margin)
+//    .attr("y1", margin)
+//    .attr("y2", svgHeight - margin)
+//    .attr("stroke", "black")
 
-} 
+// /* axis numbers */
+
+// for (let i = 1; i <= 5; i++) {
+// svg.append("text")
+//    .attr("x", margin - 10)
+//    .attr("y", yScale(i))
+//    .attr("text-anchor", "end")
+//    .attr("alignment-baseline", "middle")
+//    .text(i)
+
+// } 
+
+// for (let i = 0; i <= 160; i += 20) {
+
+// svg.append("text")
+//    .attr("x", xScale(i))
+//    .attr("y", svgHeight - margin + 15)
+//    .attr("text-anchor", "middle")
+//    .text(i)
+
+// } 
 
 /* Data points traingle */
 svg.selectAll(".dataTriangle")
    .data(dataset)
    .join("path") 
    .attr("class", "dataTriangle")
-   .attr("d", d3.symbol()
+   .attr("d", d3.symbol() // this part creates the actual triangle shape
         .type(d3.symbolTriangle)
         .size(120))
-    .attr("transform", function(d) { //positioning each trianglle using the transform attribute
+    .attr("transform", function(d) { //positioning each triangle using the transform attribute
 
 let x = xScale(d.screenTime)
 let y = yScale(d.sleepQuality)
@@ -137,7 +159,7 @@ else{
 })
 .attr("opacity", 0.8)
 
-/*draw line showing the 60 minute screen time threshold */
+/*60 minutes threshold line */
 
 svg.append("line")
    .attr("x1", xScale(60))
@@ -150,38 +172,43 @@ svg.append("line")
 /*label for the horizontal axis */
 
 svg.append("text")
-   .attr("x", svgWidth/2)
-   .attr("y", svgHeight - margin/3)
+   .attr("x", svgWidth/2) 
+   .attr("y", svgHeight - margin/6) 
    .attr("text-anchor", "middle")
    .text("Screen Time Before Bed (minutes)")
 
 /*label for the vertical axis */
 
 svg.append("text")
-   .attr("x", -svgHeight/2)
-   .attr("y", margin/3)
-   .attr("transform", "rotate(-90)")
+   .attr("transform", "translate(20, " + (svgHeight/2) + ") rotate(-90)")
    .attr("text-anchor", "middle")
    .text("Sleep Quality (1-5 scale)")
 
-/* legend title */
+svg.append("text")
+   .attr("x", -svgHeight/2) 
+   .attr("y", margin/2)
+   .attr("transform", "rotate(-90)") //-90 lets us read from bottom to top
+   .attr("text-anchor", "middle")
+   .text("Sleep Quality (1-5 scale)") 
+
+/* legend title - for the triangles */
 
 svg.append("text")
-   .attr("x", svgWidth-margin-90)
-   .attr("y", margin)
-   .attr("font-size", "1rem")
+   .attr("x", svgWidth-margin-95)
+   .attr("y", margin+2)
+   .attr("font-size", "0.8rem")
    .text("Triangle shows:")
 
 /*Triangle for sleep >= 7 hours */
 
 svg.append("path")
    .attr("d", d3.symbol().type(d3.symbolTriangle).size(120))
-   .attr("transform", "translate("+(svgWidth-margin-90)+", "+(margin+30)+")") // positioning the triangle right inside the legend area
+   .attr("transform", "translate("+(svgWidth-margin-85)+", "+(margin+30)+")") // positioning the triangle right inside the legend area
    .attr("fill","#fa5603ed")
 
 svg.append("text")
    .attr("x", svgWidth-margin-70)
-   .attr("y", margin+35)
+   .attr("y", margin+32)
    .attr("font-size", "0.8rem")
    .text(">=7 hrs sleep")
 
@@ -189,7 +216,7 @@ svg.append("text")
 
 svg.append("path")
    .attr("d", d3.symbol().type(d3.symbolTriangle).size(120))
-   .attr("transform", "translate("+(svgWidth-margin-90)+", "+(margin+60)+") rotate(180)") 
+   .attr("transform", "translate("+(svgWidth-margin-85)+", "+(margin+60)+") rotate(180)") 
    .attr("fill","#19468cf2")
 
 svg.append("text")
@@ -201,9 +228,9 @@ svg.append("text")
 /* box around legend */
 
 svg.append("rect")
-   .attr("x", svgWidth - margin - 100)
+   .attr("x", svgWidth - margin - 105)
    .attr("y", margin - 15)
-   .attr("width", 120)
+   .attr("width", 130)
    .attr("height", 90)
    .attr("fill", "none")
    .attr("stroke", "black")
@@ -211,9 +238,9 @@ svg.append("rect")
 /* dashed line legend */
 
 svg.append("rect")
-   .attr("x", svgWidth - margin - 100)
+   .attr("x", svgWidth - margin - 105)
    .attr("y", margin + 90)
-   .attr("width", 120)
+   .attr("width", 130)
    .attr("height", 35)
    .attr("fill", "none")
    .attr("stroke", "black")
